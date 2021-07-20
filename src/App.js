b0b006302a8a45018201de12d6fa6a1944214c52
Srikant <srikanta@repletech.com>
2021-07-21 03:01:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <div>
+        <header className="App-header">
+      <b>To do List</b>
+      </header>
+    <div className="wrapper">
+   
+      <Form/>
+    </div>
+    </div>
+  );
+}
+
+export default App;
